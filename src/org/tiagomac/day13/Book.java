10e7f081bddfdca69a9e87957a661cd2d75d9fc9
Cunha <tiago.cunha@capgemini.com>
2018-07-06 11:58:09 -0300

    Added challenge day 13

---
+++ src/org/tiagomac/day13/Book.java
@@ -0,0 +1,15 @@
+package org.tiagomac.day13;
+
+abstract class Book {
+
+	String title;
+	String author;
+
+	Book(String title, String author) {
+		this.title = title;
+		this.author = author;
+	}
+
+	abstract void display();
+
+}
