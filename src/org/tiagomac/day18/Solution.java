e0c984e3f63c1888e954c5195caa4d9b8b06379d
Cunha <tiago.cunha@capgemini.com>
2018-07-18 17:39:00 -0300

    day 20 and going on

---
+++ src/org/tiagomac/day18/Solution.java
@@ -0,0 +1,63 @@
+package org.tiagomac.day18;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+
+	Queue<Character> queue = new LinkedList<>();
+	Stack<Character> stack = new Stack<>();
+
+	public Solution() {
+
+	}
+
+	private Object dequeueCharacter() {
+		return this.queue.poll();
+	}
+
+	private Object popCharacter() {
+		return this.stack.pop();
+	}
+
+	private void enqueueCharacter(char c) {
+		queue.add(c);
+	}
+
+	private void pushCharacter(char c) {
+		this.stack.push(c);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String input = scan.nextLine();
+		scan.close();
+
+		// Convert input String to an array of characters:
+		char[] s = input.toCharArray();
+
+		// Create a Solution object:
+		Solution p = new Solution();
+
+		// Enqueue/Push all chars to their respective data structures:
+		for (char c : s) {
+			p.pushCharacter(c);
+			p.enqueueCharacter(c);
+		}
+
+		// Pop/Dequeue the chars at the head of both data structures and compare them:
+		boolean isPalindrome = true;
+		for (int i = 0; i < s.length / 2; i++) {
+			if (p.popCharacter() != p.dequeueCharacter()) {
+				isPalindrome = false;
+				break;
+			}
+		}
+
+		// Finally, print whether string s is palindrome or not.
+		System.out.println("The word, " + input + ", is " + ((!isPalindrome) ? "not a palindrome." : "a palindrome."));
+	}
+
+}
